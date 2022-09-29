08c8c8804b51998dd6767d5a378cc2356f9b05b5
german <gecoral@iemsantateresita.edu.co>
2022-09-29 13:03:08 -0500

    pantalla inicio

--- app/src/main/java/com/stasoft/inglishadventure/MainActivity.kt
+++ app/src/main/java/com/stasoft/inglishadventure/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.stasoft.inglishadventure
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stasoft.inglishadventure.databinding.ActivityMain2Binding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivity2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = MainActivity2()
+        setContentView(R.layout.activity_main2)
+
+        binding.buttonIniciarPantalla.setOnclickListener() {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
