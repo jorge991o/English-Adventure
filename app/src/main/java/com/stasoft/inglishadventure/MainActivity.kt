4c4b70f5f9de0babbedf246f73a463f75dcd314f
Kevin Buesaquillo <ksbuesaquillo@iemsantateresita.edu.co>
2022-09-29 13:11:43 -0500

    Activity Iniciar Sesion

--- app/src/main/java/com/stasoft/inglishadventure/MainActivity.kt
+++ app/src/main/java/com/stasoft/inglishadventure/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.stasoft.inglishadventure
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stasoft.inglishadventure.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+        private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.buttonSiguiente.setOnClickListener(){
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
     }
 }
